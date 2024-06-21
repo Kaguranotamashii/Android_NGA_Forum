69417c4e0ab32a3af3d988b40281ba21e9165cdf
宋宇然 <5592286+song_yu_ran@user.noreply.gitee.com>
2024-06-21 14:09:31 +0800

    初始化项目配置

---
+++ app/src/main/java/com/example/my_nga_fornums/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.my_nga_fornums;
+
+import android.os.Bundle;
+
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+
+
+
+public class MainActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
