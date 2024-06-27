d89d198ac77b6980b8fe5817da022edc406b9adb
Swclit011065 <1660381546@qq.com>
2024-06-27 15:51:55 +0800

    优化了登陆注册界面

--- app/src/main/java/com/example/my_nga_fornums/MainActivity.java
+++ app/src/main/java/com/example/my_nga_fornums/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.my_nga_fornums;
 
 import android.os.Bundle;
-
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -17,8 +20,13 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-
+        setContentView(R.layout.activity_login);
+        Button button1 =(Button) findViewById(R.id.login_on);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this,"11",Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
