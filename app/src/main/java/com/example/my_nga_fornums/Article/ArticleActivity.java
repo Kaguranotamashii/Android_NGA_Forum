3c8a00d800985d4f656a56e7400fce6fd008c112
limuru11 <3394711400@qq.com>
2024-07-03 14:23:29 +0800

    添加了注释

--- app/src/main/java/com/example/my_nga_fornums/Article/ArticleActivity.java
+++ app/src/main/java/com/example/my_nga_fornums/Article/ArticleActivity.java
@@ -33,10 +33,13 @@ public class ArticleActivity extends BaseActivity {
         userIdNumber = getIntent().getStringExtra("user_article_id");
         // 测试 TODO
         System.out.println("当前用户账号为：" + userIdNumber);
+
+        // 设置工具栏
         Toolbar toolbar = findViewById(R.id.article_toolbar);
         toolbar.setTitle("我的文章");
         setSupportActionBar(toolbar);
-        // 获取到ActionBar的实例
+
+        // 启用HomeAsUp按钮
         ActionBar actionBar = getSupportActionBar();
         if(actionBar != null){
             actionBar.setDisplayHomeAsUpEnabled(true);
@@ -48,17 +51,20 @@ public class ArticleActivity extends BaseActivity {
         editBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                // 跳转到编辑文章页面
                 Intent editArticleIntent = new Intent(ArticleActivity.this, EditArticleActivity.class);
                 editArticleIntent.putExtra("userId", userIdNumber);
                 startActivityForResult(editArticleIntent, 7);
             }
         });
+        // 刷新文章列表
         recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
         LinearLayoutManager layoutManager = new LinearLayoutManager(this);
         recyclerView.setLayoutManager(layoutManager);
         InitArticles();
     }
 
+    // 初始化文章列表的方法
     private void InitArticles(){
         articleList.clear();
         List<Article> articles = LitePal.where("userId = ?", userIdNumber).find(Article.class);
@@ -68,6 +74,7 @@ public class ArticleActivity extends BaseActivity {
         adapter.notifyDataSetChanged();
     }
 
+    // 处理菜单项点击事件
     public boolean onOptionsItemSelected(MenuItem item){
         switch (item.getItemId()) {
             case android.R.id.home:
@@ -77,6 +84,7 @@ public class ArticleActivity extends BaseActivity {
         return true;
     }
 
+    // 接收返回结果
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
